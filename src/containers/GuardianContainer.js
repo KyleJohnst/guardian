166b45227e950c18408b6af871c61e1f04dedc88
Dephrase <davidjf123@gmail.com>
2021-06-18 12:54:47 +0100

    List of related articles is returned to state

--- src/containers/GuardianContainer.js
+++ src/containers/GuardianContainer.js
@@ -1,13 +1,24 @@
 import SearchArticles from "../components/SearchArticles";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const GuardianContainer = () => {
 
     const [searchTerm, setSearchTerm] = useState("");
+    const [allArticles, setAllArticles] = useState([]);
 
     const onSearchChange = (search) => {
       setSearchTerm(search)
     }
+
+    const getArticles = () => {
+        fetch(`https://content.guardianapis.com/search?q=${searchTerm}&format=json&api-key=test`)
+        .then(results => results.json()).then(articles => setAllArticles(articles))
+    }
+
+    useEffect(() => {
+        getArticles();
+    }, [searchTerm])
+
 
 
     return (
