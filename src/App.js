69fa30b6b044c687ca334162747848e8ef56d1e2
Dephrase <davidjf123@gmail.com>
2021-06-18 10:49:07 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Guardian Search</h1>
+      <form>
+      <label for="input">Search for Guardian articles: </label>
+      <input type="text" placeholder="Search Term"></input>
+      <input type="submit"></input>
+      </form>
     </div>
   );
 }
